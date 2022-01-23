c2714d6eb67e3c33b1e6e15911c62f71c6e01056
VSVEL <sakiakdevi505@gmail.com>
2022-01-23 12:56:47 +0530

    Second commit.

--- main.py
+++ main.py
@@ -16,23 +16,25 @@ screen.listen()
 screen.onkey(player.go_up,"Up")
 
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+def turtle_game():
+    game_is_on = True
+    while game_is_on:
+        time.sleep(0.1)
+        screen.update()
 
-    car_manager.create_car()
-    car_manager.move_cars()
+        car_manager.create_car()
+        car_manager.move_cars()
 
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
+        for car in car_manager.all_cars:
+            if car.distance(player) < 20:
+                game_is_on = False
+                scoreboard.game_over()
 
-    if player.is_at_finish_line():
-        player.goto_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+        if player.is_at_finish_line():
+            player.goto_start()
+            car_manager.level_up()
+            scoreboard.increase_level()
 
+    screen.exitonclick()
 
-screen.exitonclick()
+turtle_game()
